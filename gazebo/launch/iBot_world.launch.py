e5a4a49b3fa71cf94f97b0791b78fec18662d17a
sagar-dimri <sagardimri98@gmail.com>
2021-11-08 19:29:31 +0530

    iBot_v1 integrated

---
+++ gazebo/launch/iBot_world.launch.py
@@ -0,0 +1,52 @@
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+# this is the function launch  system will look for
+
+def generate_launch_description():
+
+    robot_name = 'gazebo'  # robot is the name of the package
+    world_file_name = 'empty_area.world'
+
+    # full  path to urdf and world file
+    
+    world = os.path.join(get_package_share_directory(robot_name), 'worlds', world_file_name)
+
+    sdf = os.path.join(get_package_share_directory(robot_name), 'models', 'iBot', 'model.sdf')
+    
+    # read urdf contents because to spawn an entity in 
+    # gazebo we need to provide entire sdf as string on  command line
+
+    xml = open(sdf, 'r').read()
+
+    # double quotes need to be with escape sequence
+    xml = xml.replace('"', '\\"')
+
+    # this is argument format for spwan_entity service 
+    spawn_args = {"name": "iBot", "xml": xml}
+    #name = "sdf_ball"
+    #xml_cmd = "<?xml version=\"1.0\" ?><sdf version=\"1.5\"><model name=\"will_be_ignored\"><static>true</static><link name=\"link\"><visual name=\"visual\"><geometry><sphere><radius>1.0</radius></sphere></geometry></visual></link></model></sdf>"
+    #spawn_args = {"name": "sdf_ball", "xml": xml_cmd}
+
+    # create and return launch description object
+    return LaunchDescription([
+
+        # start gazebo, notice we are using libgazebo_ros_factory.so instead of libgazebo_ros_init.so
+        # That is because only libgazebo_ros_factory.so contains the service call to /spawn_entity
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+
+        # tell gazebo to spwan your robot in the world by calling service
+        ExecuteProcess(
+            cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', str(spawn_args)],
+            output='screen'),
+    ])
+
+
--- path_planning/python_controller/python_controller/python_controller.py
+++ path_planning/python_controller/python_controller/python_controller.py
@@ -8,7 +8,7 @@ class pythonController(Node):
 
     def __init__(self):
         super().__init__('python_controller')
-        self.publisher_ = self.create_publisher(Twist, '/dolly/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/iBot/cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
