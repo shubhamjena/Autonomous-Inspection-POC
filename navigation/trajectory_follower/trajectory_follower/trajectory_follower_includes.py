230782d725ac4df22bafcc8ca1f15391d24dd37c
Atharva Umbarkar <atharvaumbarkar1@gmail.com>
2022-07-12 14:53:47 +0530

    Add Comments to Navigation

--- navigation/trajectory_follower/trajectory_follower/trajectory_follower_includes.py
+++ navigation/trajectory_follower/trajectory_follower/trajectory_follower_includes.py
@@ -9,13 +9,8 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 
-from geometry_msgs.msg import Vector3
-from geometry_msgs.msg import PoseArray
-from geometry_msgs.msg import Pose
-from std_msgs.msg import Bool  # can give string if needed
-from sensor_msgs.msg import JointState
-
 reference_position = [5.0, 0.0, 0.0]
+correction_factor = 5
 n_hat = [1.0, 0.0, 0.0]
 yaw_correction = 0.0
 ekf_output = Odometry()
--- navigation/trajectory_follower/trajectory_follower/trajectory_follower_lib.py
+++ navigation/trajectory_follower/trajectory_follower/trajectory_follower_lib.py
@@ -45,26 +45,51 @@ def imu_callback(self, data):
 #* PUBLISHER CALLBACKS
 
 def velocity_timer_callback(self):
+    global correction_factor
     twist = Twist()
+
+    ''' The bot will always move forward with a constant velocity '''
     twist.linear.x = +0.1
-    twist.angular.z = yaw_correction*5
+
+    ''' The Yaw of the bot is corrected accordig to yaw_correctionand correction_factor '''
+    twist.angular.z = yaw_correction*correction_factor
+
     self.velocity_publisher.publish(twist)
 
 #* EXTRA CALLBACKS
 
 def correct_traj_callback(self):
     global orientation_unit, n_hat, yaw_correction
+
+    ''' Get the projection of orientation vector on the n_hat (yz plane) vector '''
     projected_vector = orientation_unit -  np.dot(orientation_unit, n_hat)*np.array(n_hat)
+
+    ''' Get the x-correction vector from odometry data '''
     x_correction = [5 - ekf_output.pose.pose.position.x, 0.0, 0.0]
+
+    ''' Get net correction vector '''
     net_correction = projected_vector + x_correction
+
+    ''' Make the vectors zero if very close to zero. To avoid unnecessary signs errors while calculating angle '''
     orientation_unit = make_zero(orientation_unit)
     net_correction = make_zero(net_correction)
+
     cross_product = np.cross(orientation_unit, net_correction)
     correction_angle = angle_between(net_correction, orientation_unit)
+
+    ''' Reject NaN correction angles '''
     if(correction_angle == np.nan):
         correction_angle = 0
-    yaw_correction = 1 
+    
+    ''' Initialize Yaw '''
+    yaw_correction = 1
+    
     if abs(5 - ekf_output.pose.pose.position.x) <= 0.05:
+        ''' 
+        If the bot is situated in the x = 5 +- 0.05 meters strip, 
+        just rotate the bot to correct its yaw,
+        according to its orientation and position 
+        ''' 
         if(x_correction[0] > 0):
             if x_correction[0]*orientation_unit[0] > 0:
                 yaw_correction = 1
@@ -77,22 +102,31 @@ def correct_traj_callback(self):
                 yaw_correction = 1
         yaw_correction = yaw_correction*abs(orientation_unit[0]) * (np.sign(orientation[1]))
     else:
-        # if abs(x_correction[0]) <= 0.01:
-        #     yaw_correction = yaw_correction*abs(correction_angle)*abs(x_correction[0]) * (np.sign(orientation[1])) * -1
-        # else:
-        #     yaw_correction = yaw_correction*abs(correction_angle)*abs(x_correction[0]) * (np.sign(orientation[1])) * (-np.sign(x_correction[0]))
+        '''
+        Depending on the net correction vector and the heading of the bot (orientation vector),
+        assign appropriate magnitude and direction to the yaw,
+        in this we have implemented a simple proportional magnitude  
+        '''
         if cross_product[2] <= 0:
             yaw_correction = -yaw_correction*abs(correction_angle)*abs(x_correction[0])
         else:
             yaw_correction = yaw_correction*abs(correction_angle)*abs(x_correction[0])
-    # print(correction_angle)
+
+    '''
+    Check if the bot is inverted or not,
+    on the basis of its position wrt COM of the Cessna Plane (currently 6.5),
+    and invert the yaw to rectify yaw direction whilst inverted
+    '''
     if estimated_pose.pose.pose.position.z - 6.5 <= 0:
         yaw_correction = -yaw_correction
+
     print(yaw_correction)
 
 
+# Helper Functions
+
 def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
+    """ Returns the unit vector of the input vector.  """
     if vector[0] == 0 and vector[1] == 0 and vector[2] == 0:
         return vector
     else:
@@ -113,6 +147,7 @@ def angle_between(v1, v2):
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -10.0, 10.0))
 
 def make_zero(vector):
+    ''' Makes the vector zero if it is close to zero '''
     for i in range(len(vector)):
         if abs(vector[i]) <= 0.05:
             vector[i] = 0
